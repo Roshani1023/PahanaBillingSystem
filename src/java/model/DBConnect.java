60c41e2739f937468c73d91b7fc8f24bfa365d26
Roshani <roshanikka1023@gmail.com>
2025-07-28 05:48:03 +0530

    Database connection

---
+++ src/java/model/DBConnect.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class DBConnect {
+     public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/pahana_billing", "root", "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
