27f7f26e796ed5c7b050b89dee553c7378bc463c
Roshani <roshanikka1023@gmail.com>
2025-07-28 05:48:26 +0530

    Login - Backend

---
+++ src/java/controller/LoginServlet.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.DBConnect;
+import java.io.IOException;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+@WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
+public class LoginServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        try (Connection conn = DBConnect.getConnection()) {
+            String sql = "SELECT * FROM admin WHERE username=? AND password=?";
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1, username);
+            pst.setString(2, password);
+
+            ResultSet rs = pst.executeQuery();
+
+            if (rs.next()) {
+                HttpSession session = request.getSession();
+                session.setAttribute("username", username);
+                response.sendRedirect("dashboard.jsp");
+            } else {
+                HttpSession session = request.getSession();
+                session.setAttribute("error", "Invalid credentials");
+                response.sendRedirect("login.jsp");
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.sendRedirect("login.jsp?error=Server+error");
+        }
+    }
+}
